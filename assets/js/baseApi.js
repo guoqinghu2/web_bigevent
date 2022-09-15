a51609170f453bfa896c22194b7b90eda147eba6
guoqinghu2 <1247515982@qq.com>
2022-09-15 19:54:48 +0800

    完成了登录和注册功能的开发

---
+++ assets/js/baseApi.js
@@ -0,0 +1,9 @@
+// 注意 每次调用$.ajax()或者$.post()或者$.get()时
+// 会调用这个函数
+// 在这个函数中  我们把根目录存进来，会自动给我们拼接一个完整请求地址
+可以拿到给ajax提供的配置对象
+$.ajaxPrefilter(function(options) {
+
+    options.url = 'http://www.liulongbin.top:3007' + options.url
+    console.log(options.url);
+})
---
+++ assets/js/login.js
@@ -0,0 +1,106 @@
+$(function() {
+
+    // 点击去注册账号的链接
+    $('#link_reg').on('click', function() {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+
+    // 点击去登录的链接
+    $('#link_login').on('click', function() {
+        $('.reg-box').hide()
+        $('.login-box').show()
+    })
+
+    // 表单验证
+    // 首先从layui上获取 form 对象
+    let form = layui.form
+
+    // 到处弹出层
+    let layer = layui.layer
+
+    // 校验密码
+    // 通过form.verify()函数 自定义校验规则
+    form.verify({
+        // 自定义了一个叫做pwd的校验规则  用于校验密码
+        'pwd': [
+            /^[\S]{6,12}$/,
+            '密码必须6到12位，且不能出现空格'
+        ],
+        // 校验两次密码是否一致的规则
+        'repwd': function(value) {
+            let pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return '两次密码不一致 请重新输入'
+            }
+        }
+    })
+
+
+    // 注册模块
+    // 监听注册表单的提交事件
+    $('#form_reg').on('submit', function(e) {
+        // 阻止表单默认提交
+        e.preventDefault()
+
+        // 获取到表单输入的注册用户名和密码
+        let data = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }
+
+        // 发送一个携带了注册用户名和密码的post请求
+        $.post('api/reguser', data, function(res) {
+
+            // 请求失败
+            if (res.status !== 0) {
+                return layer.msg('注册失败' + res.message)
+            }
+            layer.msg('注册成功 请登录')
+
+            // 注册成功后 自动跳转到登录页面
+            // 获取到跳转登录按钮 然后模拟人的点击行为
+            $('#link_login').click()
+
+        })
+    })
+
+
+    // 登录模块
+    // 监听登录表单的提交事件
+    $('#form_login').submit(function(e) {
+        // 阻止默认提交
+        e.preventDefault()
+
+        // 获取表单中提交的用户名和数据
+
+
+        // 发起一个携带用户名和密码的post请求
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+
+            //使用jquery中打serialize()  获取表单中的数据
+            data: $(this).serialize(),
+            success: function(res) {
+
+                // 登录失败
+                if (res.status !== 0) {
+                    return layer.msg('登录失败')
+                }
+
+                // 登录成功
+                layer.msg('登录成功')
+
+                // 跳转到首页
+                // location.href = '../大事件后后台管理项目/home/dashboard.html'
+            }
+        })
+
+
+    })
+
+
+
+
+})
